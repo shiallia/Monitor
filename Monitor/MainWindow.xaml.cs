935372b15079d209637329b3f669a1edeb9f079f
zhangfangithub <magiczhang@outlook.com>
2016-05-18 11:05:22 +0800

    添加了一些基础的代码

--- Monitor/MainWindow.xaml.cs
+++ Monitor/MainWindow.xaml.cs
@@ -1,28 +1,186 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Management;
+//using System.Net;
+//using System.Net.NetworkInformation;
+//using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 
-namespace Monitor
+namespace DeviceManager
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ulong totMem;
+        Monitor util;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filepath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filePath);
+
+        private string strFilePath = @"d:/clent/config/devicetag.ini";//获取INI文件路径
+
+        public static void CopyDir(string srcPath, string aimPath)
+        {
+            try
+            {
+                // 检查目标目录是否以目录分割字符结束如果不是则添加之
+                if (aimPath[aimPath.Length - 1] != Path.DirectorySeparatorChar)
+                    aimPath += Path.DirectorySeparatorChar;
+                // 判断目标目录是否存在如果不存在则新建之
+                if (!Directory.Exists(aimPath))
+                    Directory.CreateDirectory(aimPath);
+                // 得到源目录的文件列表，该里面是包含文件以及目录路径的一个数组
+                // 如果你指向copy目标文件下面的文件而不包含目录请使用下面的方法
+                // string[] fileList = Directory.GetFiles(srcPath);
+                string[] fileList = Directory.GetFileSystemEntries(srcPath);
+                // 遍历所有的文件和目录
+                foreach (string file in fileList)
+                {
+                    // 先当作目录处理如果存在这个目录就递归Copy该目录下面的文件
+                    if (Directory.Exists(file))
+                        CopyDir(file, aimPath + Path.GetFileName(file));
+                    // 否则直接Copy文件
+                    else
+                        File.Copy(file, aimPath + Path.GetFileName(file), true);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("还原失败", "系统还原", MessageBoxButton.OKCancel);
+            }
+        }
+
+        public static void Kill(string name)
+        {
+            Process[] process;//创建一个PROCESS类数组
+            process = Process.GetProcesses();//获取当前任务管理器所有运行中程序
+            foreach (Process proces in process)//遍历
+            {
+                if (proces.ProcessName == name)
+                {
+                    proces.Kill();
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            util = new Monitor();
+            util.callback += Callback;
+            totMem = util.GetPhysicalMemSize();
+            System.Threading.ThreadPool.QueueUserWorkItem(WorkerItem);
+            memTotal.Text = ConvertBytes(totMem, 3) + " GB";
+        }
+
+        void WorkerItem(object arg)
+        {
+            util.Run();
+        }
+
+        void Callback(float cpu, ulong mem, string ip, string yzdmem)
+        {
+            var pcpu = Math.Round(cpu, 2, MidpointRounding.AwayFromZero);
+            var used_mem = totMem - mem;
+            var mb_mem = ConvertBytes(used_mem, 2);
+            var pmem = ConvertBytes(Convert.ToUInt64(mb_mem), 1);
+
+            Dispatcher.Invoke((Action)(() =>
+            {
+                cpuT.Text = pcpu + "%";
+                cpuP.Value = pcpu;
+                memT.Text = String.Format("内存总占用：{0} GB ({1:N} MB)", pmem, mb_mem);
+                yzdmemT.Text = "会议程序占用：" + yzdmem + "MB";
+                memP.Value = (double)(used_mem / Convert.ToDecimal(totMem) * 100);
+                ipT.Text = ip;
+            }));
+        }
+
+        decimal ConvertBytes(ulong b, int iteration)
+        {
+            long iter = 1;
+            for (int i = 0; i < iteration; i++)
+                iter *= 1024;
+            return Math.Round((Convert.ToDecimal(b)) / Convert.ToDecimal(iter), 2, MidpointRounding.AwayFromZero);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            util.Dispose();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //SettingWindow settingWindow = new SettingWindow();
+            //settingWindow.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("本操作将还原业务程序，并重新启动设备", "系统还原", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                Kill("cmd");
+                Kill("client");
+                Kill("YSTenLoader");
+                System.Threading.Thread.Sleep(1000);
+                CopyDir(@"c:/bak", "d:/");
+                System.Diagnostics.Process.Start("shutdown", @"/r /t 0");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("本操作将提交对c盘的更改，并重新启动设备", "ewf提交", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                System.Diagnostics.Process.Start("commit.bat");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("本操作将会将程序版本号归0，并重新启动设备", "系统还原", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                WritePrivateProfileString("Device", "Pkg_VersionNum", "0", strFilePath);
+                WritePrivateProfileString("Device", "Pkg_Version", "0", strFilePath);
+                System.Diagnostics.Process.Start("shutdown", @"/r /t 0");
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Border_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
---
+++ Monitor/Monitor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Diagnostics;
+//using System.Management;
+using Microsoft.VisualBasic.Devices;
+using System.Net.NetworkInformation;
+
+namespace DeviceManager
+{
+    class Monitor : IDisposable
+    {
+        public delegate void CallbackEventHandler(float a, ulong b, string c, string d);
+
+        public event CallbackEventHandler callback;
+
+        public ulong GetPhysicalMemSize()
+        {
+            return cinf.TotalPhysicalMemory;
+        }
+
+        PerformanceCounter cpu;
+        ComputerInfo cinf;
+
+        public Monitor()
+        {
+            cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            cinf = new ComputerInfo();
+            
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                //cpu占用
+                var percentage = cpu.NextValue();
+                //内存占用
+                var mem = cinf.AvailablePhysicalMemory;
+
+                //当前ip
+                string localIP = string.Empty;
+                NetworkInterface[] interfaces = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+                int len = interfaces.Length;
+                for (int i = 0; i < len; i++)  
+                {
+                    NetworkInterface ni = interfaces[i];
+                    if (ni.Name == "WLAN")
+                    {
+                        IPInterfaceProperties property = ni.GetIPProperties();
+                        foreach (UnicastIPAddressInformation ip in property.UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                localIP = ip.Address.ToString();                                
+                            }
+                        }
+                    }
+                }
+
+                //client进程内存占用
+                Process[] myPro = Process.GetProcessesByName("client");
+                string yzdMem = string.Empty;
+                if(myPro.Length != 0)
+                {
+                    yzdMem = (myPro[0].PrivateMemorySize64 / 1024 / 1024).ToString();
+                }
+                
+                callback(percentage, mem , localIP , yzdMem);
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        public void Dispose()
+        {
+            cpu.Dispose();
+        }
+    }
+}
